a1f3ba938171fc159ba04b671f17f4aa6dc0ef45
Ocelot5836 <FinntheRaider@gmail.com>
2018-08-26 11:41:14 -0700

    mostly implemented the default wolf render

--- src/main/java/com/ocelot/betteranimals/client/RenderHandler.java
+++ src/main/java/com/ocelot/betteranimals/client/RenderHandler.java
@@ -7,6 +7,7 @@ import com.ocelot.betteranimals.client.render.RenderNewPig;
 import com.ocelot.betteranimals.client.render.RenderNewSheep;
 import com.ocelot.betteranimals.client.render.RenderNewSilverfish;
 import com.ocelot.betteranimals.client.render.RenderNewSpider;
+import com.ocelot.betteranimals.client.render.RenderNewWolf;
 
 import net.minecraft.entity.monster.EntityCaveSpider;
 import net.minecraft.entity.monster.EntitySilverfish;
@@ -15,6 +16,7 @@ import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class RenderHandler {
@@ -24,6 +26,7 @@ public class RenderHandler {
 		RenderingRegistry.registerEntityRenderingHandler(EntityPig.class, new RenderNewPig());
 		RenderingRegistry.registerEntityRenderingHandler(EntityChicken.class, new RenderNewChicken());
 		RenderingRegistry.registerEntityRenderingHandler(EntitySheep.class, new RenderNewSheep());
+		RenderingRegistry.registerEntityRenderingHandler(EntityWolf.class, new RenderNewWolf());
 
 		// Mobs
 		RenderingRegistry.registerEntityRenderingHandler(EntitySpider.class, new RenderNewSpider());
