04c257bbceb7f0aaa858c2600228eab60fb491e4
itsmeow <mods.itsmeow@gmail.com>
2019-05-04 21:08:59 -0400

    Set version 5.0

--- src/main/java/com/ocelot/betteranimals/BetterAnimals.java
+++ src/main/java/com/ocelot/betteranimals/BetterAnimals.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class BetterAnimals {
 
 	public static final String MODID = "betteranimals";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "@VERSION@";
 
 	@Instance(MODID)
 	public static BetterAnimals instance;
