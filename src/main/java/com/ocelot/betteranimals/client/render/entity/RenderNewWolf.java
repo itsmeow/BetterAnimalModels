93b5aaf3b883a5769ac270a5c8835e2e9dd2432f
itsmeow <mods.itsmeow@gmail.com>
2019-06-09 20:15:56 -0400

    Add collars to wolves

--- src/main/java/com/ocelot/betteranimals/client/render/entity/RenderNewWolf.java
+++ src/main/java/com/ocelot/betteranimals/client/render/entity/RenderNewWolf.java
@@ -2,6 +2,7 @@ package com.ocelot.betteranimals.client.render.entity;
 
 import com.ocelot.betteranimals.BetterAnimals;
 import com.ocelot.betteranimals.client.model.ModelNewWolf;
+import com.ocelot.betteranimals.client.render.layer.LayerNewWolfCollar;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
@@ -11,44 +12,46 @@ import net.minecraft.util.ResourceLocation;
 
 public class RenderNewWolf extends RenderLiving<EntityWolf> {
 
-	private static final ResourceLocation WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf.png");
-	private static final ResourceLocation TAMED_WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf_tame.png");
-	private static final ResourceLocation ANRGY_WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf_angry.png");
+    private static final ResourceLocation WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf.png");
+    private static final ResourceLocation TAMED_WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf_tame.png");
+    private static final ResourceLocation ANRGY_WOLF_TEXTURES = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/wolf/wolf_angry.png");
 
-	public RenderNewWolf(RenderManager m) {
-		super(m, new ModelNewWolf(), 0.25f);
-	}
+    public RenderNewWolf(RenderManager m) {
+        super(m, new ModelNewWolf(), 0.25f);
+        this.addLayer(new LayerNewWolfCollar(this));
+    }
 
-	@Override
-	protected float handleRotationFloat(EntityWolf livingBase, float partialTicks) {
-		return livingBase.getTailRotation();
-	}
-	
-	@Override
-	protected void preRenderCallback(EntityWolf entitylivingbaseIn, float partialTickTime) {
-		if (getMainModel().isChild) {
-			GlStateManager.scale(0.5D, 0.5D, 0.5D);
-		} else {
-			GlStateManager.scale(1.0D, 1.0D, 1.0D);
-		}
-	}
+    @Override
+    protected float handleRotationFloat(EntityWolf livingBase, float partialTicks) {
+        return livingBase.getTailRotation();
+    }
 
-	@Override
-	public void doRender(EntityWolf entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		if (entity.isWolfWet()) {
-			float f = entity.getBrightness() * entity.getShadingWhileWet(partialTicks);
-			GlStateManager.color(f, f, f);
-		}
+    @Override
+    protected void preRenderCallback(EntityWolf entitylivingbaseIn, float partialTickTime) {
+        if (getMainModel().isChild) {
+            GlStateManager.scale(0.5D, 0.5D, 0.5D);
+        } else {
+            GlStateManager.scale(1.0D, 1.0D, 1.0D);
+        }
+    }
 
-		super.doRender(entity, x, y, z, entityYaw, partialTicks);
-	}
+    @Override
+    public void doRender(EntityWolf entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        if (entity.isWolfWet()) {
+            float f = entity.getBrightness() * entity.getShadingWhileWet(partialTicks);
+            GlStateManager.color(f, f, f);
+        }
 
-	@Override
-	protected ResourceLocation getEntityTexture(EntityWolf entity) {
-		if (entity.isTamed()) {
-			return TAMED_WOLF_TEXTURES;
-		} else {
-			return entity.isAngry() ? ANRGY_WOLF_TEXTURES : WOLF_TEXTURES;
-		}
-	}
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityWolf entity) {
+        if (entity.isTamed()) {
+            return TAMED_WOLF_TEXTURES;
+        } else {
+            return entity.isAngry() ? ANRGY_WOLF_TEXTURES : WOLF_TEXTURES;
+        }
+    }
+
 }
