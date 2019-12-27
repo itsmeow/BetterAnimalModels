c2f7c9fd13a72de53313882c8be9b9debd138a2a
itsmeow <mods.itsmeow@gmail.com>
2019-12-26 21:41:46 -0500

    Add saddles to pigs

--- src/main/java/com/ocelot/betteranimals/client/render/entity/RenderNewPig.java
+++ src/main/java/com/ocelot/betteranimals/client/render/entity/RenderNewPig.java
@@ -3,21 +3,42 @@ package com.ocelot.betteranimals.client.render.entity;
 import com.ocelot.betteranimals.BetterAnimals;
 import com.ocelot.betteranimals.client.model.ModelNewPig;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderNewPig extends RenderLiving<EntityPig> {
+public class RenderNewPig extends RenderLiving<EntityPig> implements LayerRenderer<EntityPig> {
 
-	private static final ResourceLocation BASE = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/pig.png");
+    private static final ResourceLocation BASE = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/pig.png");
+    private static final ResourceLocation SADDLE = new ResourceLocation(BetterAnimals.MODID, "textures/mobs/pig_saddle.png");
 
-	public RenderNewPig(RenderManager m) {
-		super(m, new ModelNewPig(), 0.7F);
-	}
+    public RenderNewPig(RenderManager m) {
+        super(m, new ModelNewPig(), 0.7F);
+        this.addLayer(this);
+    }
 
-	@Override
-	protected ResourceLocation getEntityTexture(EntityPig entity) {
-		return BASE;
-	}
+    @Override
+    protected ResourceLocation getEntityTexture(EntityPig entity) {
+        return BASE;
+    }
+
+    public void doRenderLayer(EntityPig entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        if(entity.getSaddled()) {
+            this.bindTexture(SADDLE);
+            GlStateManager.pushMatrix();
+            {
+                GlStateManager.scale(1.05F, 1.05F, 1.05F);
+                GlStateManager.translate(0F, -0.05F, 0F);
+                this.mainModel.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            }
+            GlStateManager.popMatrix();
+        }
+    }
+
+    public boolean shouldCombineTextures() {
+        return false;
+    }
 }
