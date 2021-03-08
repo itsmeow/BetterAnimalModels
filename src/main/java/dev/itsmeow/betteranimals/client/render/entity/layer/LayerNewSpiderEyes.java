035c52cb705c39d6c7dcdba2b4089d0a52999fbe
itsmeow <mods.itsmeow@gmail.com>
2021-03-08 05:30:18 -0500

    Fix eye glow with shaders (Fixes #34)

--- src/main/java/dev/itsmeow/betteranimals/client/render/entity/layer/LayerNewSpiderEyes.java
+++ src/main/java/dev/itsmeow/betteranimals/client/render/entity/layer/LayerNewSpiderEyes.java
@@ -3,6 +3,7 @@ package dev.itsmeow.betteranimals.client.render.entity.layer;
 import com.mojang.blaze3d.platform.GLX;
 import com.mojang.blaze3d.platform.GlStateManager;
 
+import com.mushroom.midnight.common.util.MidnightUtil;
 import dev.itsmeow.betteranimals.BetterAnimals;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
@@ -11,6 +12,7 @@ import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class LayerNewSpiderEyes<T extends LivingEntity, A extends EntityModel<T>> extends LayerRenderer<T, A> {
 
@@ -21,34 +23,27 @@ public class LayerNewSpiderEyes<T extends LivingEntity, A extends EntityModel<T>
     }
 
     @Override
-    public void render(T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        this.bindTexture(GLOW);
-        GlStateManager.enableBlend();
-        GlStateManager.disableAlphaTest();
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
-        if(entitylivingbaseIn.isInvisible()) {
-            GlStateManager.depthMask(false);
-        } else {
+    public void render(T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        if(!entity.isInvisible()) {
+            this.bindTexture(GLOW);
             GlStateManager.depthMask(true);
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+            int i = 61680;
+            int j = i % 65536;
+            int k = i / 65536;
+            GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, (float)j, (float)k);
+            GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.disableLighting();
+            this.getEntityModel().render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            GlStateManager.enableLighting();
+            i = entity.getBrightnessForRender();
+            j = i % 65536;
+            k = i / 65536;
+            GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, (float)j, (float)k);
+            this.func_215334_a(entity);
+            GlStateManager.disableBlend();
         }
-
-        int i = 61680;
-        int j = i % 65536;
-        int k = i / 65536;
-        GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, (float) j, (float) k);
-        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GameRenderer gamerenderer = Minecraft.getInstance().gameRenderer;
-        gamerenderer.setupFogColor(true);
-        this.getEntityModel().render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-        gamerenderer.setupFogColor(false);
-        i = entitylivingbaseIn.getBrightnessForRender();
-        j = i % 65536;
-        k = i / 65536;
-        GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, (float) j, (float) k);
-        this.func_215334_a(entitylivingbaseIn);
-        GlStateManager.depthMask(true);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlphaTest();
     }
 
     @Override
