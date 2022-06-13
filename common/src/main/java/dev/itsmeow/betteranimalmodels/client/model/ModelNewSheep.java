ac05598b7308bc61b4e91067272a9bd03127e9e7
itsmeow <itsmeow@itsmeow.dev>
2022-06-13 13:00:32 -0400

    Fix issues with sheep wool

--- common/src/main/java/dev/itsmeow/betteranimalmodels/client/model/ModelNewSheep.java
+++ common/src/main/java/dev/itsmeow/betteranimalmodels/client/model/ModelNewSheep.java
@@ -36,7 +36,7 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
     public ModelPart lfHoofClaw01b;
     public ModelPart lfHoofClaw02a;
     public ModelPart lfHoofClaw02b;
-    public ModelPart lForelegWool01;
+    public ModelPart lForelegWool;
     public ModelPart lForelegWool02;
     public ModelPart rForeleg01;
     public ModelPart rForeleg02;
@@ -44,8 +44,8 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
     public ModelPart rfHoofClaw01b;
     public ModelPart rfHoofClaw02a;
     public ModelPart rfHoofClaw02b;
-    public ModelPart rForelegWool01;
-    public ModelPart rForelegWool02;
+    public ModelPart rForelegWool;
+    public ModelPart lForelegWool3;
     public ModelPart neck;
     public ModelPart head;
     public ModelPart jawUpper;
@@ -53,7 +53,7 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
     public ModelPart snout;
     public ModelPart lEar;
     public ModelPart rEar2;
-    public ModelPart woolHead;
+    public ModelPart woolHead01;
     public ModelPart woolNeck;
     public ModelPart woolBody;
     public ModelPart woolRear;
@@ -87,16 +87,16 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
         this.lfHoofClaw01b = lfHoofClaw01a.getChild("lfHoofClaw01b");
         this.lfHoofClaw02a = lForeleg02.getChild("lfHoofClaw02a");
         this.lfHoofClaw02b = lfHoofClaw02a.getChild("lfHoofClaw02b");
-        this.lForelegWool01 = lForeleg01.getChild("lForelegWool01");
-        this.lForelegWool02 = lForelegWool01.getChild("lForelegWool02");
+        this.lForelegWool = lForeleg01.getChild("lForelegWool");
+        this.lForelegWool02 = lForelegWool.getChild("lForelegWool02");
         this.rForeleg01 = body.getChild("rForeleg01");
         this.rForeleg02 = rForeleg01.getChild("rForeleg02");
         this.rfHoofClaw01a = rForeleg02.getChild("rfHoofClaw01a");
         this.rfHoofClaw01b = rfHoofClaw01a.getChild("rfHoofClaw01b");
         this.rfHoofClaw02a = rForeleg02.getChild("rfHoofClaw02a");
         this.rfHoofClaw02b = rfHoofClaw02a.getChild("rfHoofClaw02b");
-        this.rForelegWool01 = rForeleg01.getChild("rForelegWool01");
-        this.rForelegWool02 = rForelegWool01.getChild("rForelegWool02");
+        this.rForelegWool = rForeleg01.getChild("rForelegWool");
+        this.lForelegWool3 = rForelegWool.getChild("lForelegWool3");
         this.neck = body.getChild("neck");
         this.head = neck.getChild("head");
         this.jawUpper = head.getChild("jawUpper");
@@ -104,7 +104,7 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
         this.snout = head.getChild("snout");
         this.lEar = head.getChild("lEar");
         this.rEar2 = head.getChild("rEar2");
-        this.woolHead = head.getChild("woolHead");
+        this.woolHead01 = head.getChild("woolHead01");
         this.woolNeck = neck.getChild("woolNeck");
         this.woolBody = body.getChild("woolBody");
         this.woolRear = woolBody.getChild("woolRear");
@@ -139,16 +139,16 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
         PartDefinition lfHoofClaw01b = lfHoofClaw01a.addOrReplaceChild("lfHoofClaw01b", CubeListBuilder.create().texOffs(9, 48).addBox(-0.49F, -1.1F, -0.3F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.5F, -1.0F, 0.3491F, 0.0F, 0.0F));
         PartDefinition lfHoofClaw02a = lForeleg02.addOrReplaceChild("lfHoofClaw02a", CubeListBuilder.create().texOffs(9, 44).addBox(-0.5F, -0.5F, -1.5F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.5F, 7.5F, -0.3F, 0.0F, 0.0698F, -0.0349F));
         PartDefinition lfHoofClaw02b = lfHoofClaw02a.addOrReplaceChild("lfHoofClaw02b", CubeListBuilder.create().texOffs(9, 48).addBox(-0.49F, -1.1F, -0.3F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.5F, -1.0F, 0.3491F, 0.0F, 0.0F));
-        PartDefinition lForelegWool01 = lForeleg01.addOrReplaceChild("lForelegWool01", CubeListBuilder.create().texOffs(16, 51).addBox(-0.6F, -2.3F, -2.1F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-        PartDefinition lForelegWool02 = lForelegWool01.addOrReplaceChild("lForelegWool02", CubeListBuilder.create().texOffs(16, 58).addBox(-2.01F, -0.1F, -2.1F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.9F, 6.8F, 0.6F, -0.0873F, 0.0F, -0.0698F));
+        PartDefinition lForelegWool = lForeleg01.addOrReplaceChild("lForelegWool", CubeListBuilder.create().texOffs(16, 51).addBox(-0.6F, -2.3F, -2.1F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition lForelegWool02 = lForelegWool.addOrReplaceChild("lForelegWool02", CubeListBuilder.create().texOffs(16, 58).addBox(-2.01F, -0.1F, -2.1F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.9F, 6.8F, 0.6F, -0.0873F, 0.0F, -0.0698F));
         PartDefinition rForeleg01 = body.addOrReplaceChild("rForeleg01", CubeListBuilder.create().texOffs(0, 33).mirror().addBox(-3.0F, -1.4F, -1.5F, 3.0F, 8.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-2.5F, 0.5F, -4.0F, 0.0873F, 0.0F, -0.0698F));
         PartDefinition rForeleg02 = rForeleg01.addOrReplaceChild("rForeleg02", CubeListBuilder.create().texOffs(0, 44).mirror().addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-1.5F, 6.5F, 0.1F, -0.0873F, 0.0F, 0.0698F));
         PartDefinition rfHoofClaw01a = rForeleg02.addOrReplaceChild("rfHoofClaw01a", CubeListBuilder.create().texOffs(9, 44).mirror().addBox(-0.5F, -0.5F, -1.5F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-0.5F, 7.5F, -0.3F, 0.0F, 0.1047F, 0.0349F));
         PartDefinition rfHoofClaw01b = rfHoofClaw01a.addOrReplaceChild("rfHoofClaw01b", CubeListBuilder.create().texOffs(9, 48).mirror().addBox(-0.51F, -1.1F, -0.3F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.5F, -1.0F, 0.3491F, 0.0F, 0.0F));
         PartDefinition rfHoofClaw02a = rForeleg02.addOrReplaceChild("rfHoofClaw02a", CubeListBuilder.create().texOffs(9, 44).mirror().addBox(-0.5F, -0.5F, -1.5F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.5F, 7.5F, -0.3F, 0.0F, -0.0698F, 0.0349F));
         PartDefinition rfHoofClaw02b = rfHoofClaw02a.addOrReplaceChild("rfHoofClaw02b", CubeListBuilder.create().texOffs(9, 48).mirror().addBox(-0.51F, -1.1F, -0.3F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.5F, -1.0F, 0.3491F, 0.0F, 0.0F));
-        PartDefinition rForelegWool01 = rForeleg01.addOrReplaceChild("rForelegWool01", CubeListBuilder.create().texOffs(16, 51).mirror().addBox(-3.4F, -2.3F, -2.1F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
-        PartDefinition rForelegWool02 = rForelegWool01.addOrReplaceChild("rForelegWool02", CubeListBuilder.create().texOffs(16, 58).mirror().addBox(-0.99F, -0.1F, -2.1F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-1.9F, 6.8F, 0.6F, -0.0873F, 0.0F, 0.0698F));
+        PartDefinition rForelegWool = rForeleg01.addOrReplaceChild("rForelegWool", CubeListBuilder.create().texOffs(16, 51).mirror().addBox(-3.4F, -2.3F, -2.1F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition lForelegWool3 = rForelegWool.addOrReplaceChild("lForelegWool3", CubeListBuilder.create().texOffs(16, 58).mirror().addBox(-0.99F, -0.1F, -2.1F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-1.9F, 6.8F, 0.6F, -0.0873F, 0.0F, 0.0698F));
         PartDefinition neck = body.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(24, 22).addBox(-2.5F, -2.5F, -4.6F, 5.0F, 5.0F, 5.0F, new CubeDeformation(-0.01F)), PartPose.offsetAndRotation(0.0F, -0.3F, -5.6F, -0.9599F, 0.0F, 0.0F));
         PartDefinition head = neck.addOrReplaceChild("head", CubeListBuilder.create().texOffs(44, 22).addBox(-2.5F, -3.0F, -3.0F, 5.0F, 5.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.5F, -4.6F, 0.9599F, 0.0F, 0.0F));
         PartDefinition jawUpper = head.addOrReplaceChild("jawUpper", CubeListBuilder.create().texOffs(48, 0).addBox(-2.0F, 0.1F, -2.7F, 4.0F, 1.0F, 3.0F, new CubeDeformation(0.01F)), PartPose.offset(0.0F, 0.0F, -2.7F));
@@ -156,8 +156,8 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
         PartDefinition snout = head.addOrReplaceChild("snout", CubeListBuilder.create().texOffs(32, 0).addBox(-2.0F, -2.1F, -3.6F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -1.1F, -2.0F, 0.6109F, 0.0F, 0.0F));
         PartDefinition lEar = head.addOrReplaceChild("lEar", CubeListBuilder.create().texOffs(32, 8).mirror().addBox(-2.7F, -0.5F, -1.0F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-2.0F, -2.1F, 0.7F, -0.5236F, 0.0F, -0.4363F));
         PartDefinition rEar2 = head.addOrReplaceChild("rEar2", CubeListBuilder.create().texOffs(32, 8).addBox(-0.3F, -0.5F, -1.0F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.0F, -2.1F, 0.7F, -0.5236F, 0.0F, 0.4363F));
-        PartDefinition woolHead = head.addOrReplaceChild("woolHead", CubeListBuilder.create().texOffs(14, 52).addBox(-3.0F, -3.0F, -1.5F, 6.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.3F, -0.1F));
-        PartDefinition woolNeck = neck.addOrReplaceChild("woolNeck", CubeListBuilder.create().texOffs(13, 50).addBox(-2.6F, -3.5F, -3.0F, 5.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.5F, -1.9F));
+        PartDefinition woolHead01 = head.addOrReplaceChild("woolHead01", CubeListBuilder.create().texOffs(14, 52).addBox(-3.0F, -3.0F, -1.5F, 6.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.3F, -0.1F));
+        PartDefinition woolNeck = neck.addOrReplaceChild("woolNeck", CubeListBuilder.create().texOffs(13, 50).addBox(-2.5F, -3.5F, -3.0F, 5.0F, 6.0F, 6.0F, new CubeDeformation(0.2F)), PartPose.offset(0.0F, 0.5F, -1.9F));
         PartDefinition woolBody = body.addOrReplaceChild("woolBody", CubeListBuilder.create().texOffs(1, 41).addBox(-4.5F, -4.5F, -9.1F, 9.0F, 9.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.9F, 3.5F));
         PartDefinition woolRear = woolBody.addOrReplaceChild("woolRear", CubeListBuilder.create().texOffs(34, 41).addBox(-4.0F, -5.3F, 0.4F, 8.0F, 8.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.3F, 2.3F, -0.384F, 0.0F, 0.0F));
         PartDefinition woolFront = woolBody.addOrReplaceChild("woolFront", CubeListBuilder.create().texOffs(11, 52).addBox(-4.55F, -3.9F, -0.3F, 9.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.4F, -10.9F));
@@ -167,17 +167,17 @@ public class ModelNewSheep<T extends Sheep> extends Model<T> {
     @Override
     public void renderToBuffer(PoseStack matrixStackIn, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
         this.tail.visible = !sheared && !young;
-        this.woolHead.visible = !sheared;
+        this.woolHead01.visible = !sheared;
         this.woolBody.visible = !sheared;
         this.woolNeck.visible = !sheared;
         this.woolRear.visible = !sheared;
         this.woolFront.visible = !sheared;
         this.lHindlegWool.visible = !sheared;
         this.rHindlegWool.visible = !sheared;
-        this.lForelegWool01.visible = !sheared;
+        this.lForelegWool.visible = !sheared;
         this.lForelegWool02.visible = !sheared;
-        this.rForelegWool01.visible = !sheared;
-        this.rForelegWool02.visible = !sheared;
+        this.rForelegWool.visible = !sheared;
+        this.lForelegWool3.visible = !sheared;
         this.body.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
     }
 
